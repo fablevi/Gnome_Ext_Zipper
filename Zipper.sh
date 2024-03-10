dbdab39ce50803d7aa76d98e1909bdb8be1926bc
Fábián Levente <97455713+fablevi@users.noreply.github.com>
2024-03-10 13:20:26 +0100

    Update Zipper.sh

--- Zipper.sh
+++ Zipper.sh
@@ -1,8 +1,62 @@
 #!/bin/bash
 
-folder=$1
-shift
-ignore=$@
+function zipperDefault(){
 
-echo "$folder elso arg"
-echo "$ignore tobbi arg"
+    files="extension.js metadata.json prefs.js schemas"
+
+    echo "Give the folder full path path"
+    echo "Example: /home/user/Desktop/folder"
+    echo ""
+    read folder;
+    echo ""
+    echo "What will be the zip file name?"
+    echo "Example: zipFileName@afterTheSymbol.iWrote.theName"
+    echo ""
+    read zipFileName;
+    echo ""
+    echo "Zipping up the folders" 
+    cd $folder
+    zip $zipFileName $files
+    mv $zipFileName.zip $(dirname $0)/zipped  
+    echo ""
+    echo "Moved to the home folder"
+}
+
+function zipperDefaultIgnore(){
+    echo ""
+}
+
+function helper(){
+    echo ""
+}
+
+
+if [ -z "$1" ]; then
+    echo "What do you want to do?";
+    echo "Zip default folder files 🙂[1]";
+    echo "Zip default files with ignores 🤔[2]";
+    echo "Get help! 📖[3]";
+    echo ""
+    read options;
+
+    case $options in
+        "1")
+            zipperDefault
+            ;;
+        "2")
+            zipperDefaultIgnore
+            ;;
+        "3")
+            helper
+            ;;
+        *)
+            echo "Unrecognised value, the program shut down."
+            ;;
+    esac
+else
+echo "hw"
+fi
+
+
+
+
